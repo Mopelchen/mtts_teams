40adbf14709aec37d05bc1fdc23d590607e6f58b
Klaus <klaus.donath@gmail.com>
2021-04-12 18:00:48 +0200

    Initial Setup

---
+++ src/test/java/com/mtts/teams/TeamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtts.teams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
